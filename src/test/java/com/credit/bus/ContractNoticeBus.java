b632a4149b66d6ff2cf37a40246d9825e7e8cbf9
lichunfeng <lichunfeng@hexindai.com>
2017-09-21 12:34:00 +0800

    sign

---
+++ src/test/java/com/credit/bus/ContractNoticeBus.java
@@ -0,0 +1,44 @@
+package com.credit.bus;
+
+import org.openqa.selenium.WebDriver;
+
+import com.credit.page.SignManagePage;
+
+public class ContractNoticeBus extends SignManagePage {
+
+	public ContractNoticeBus(WebDriver driver) {
+		super(driver);
+	}
+	/*
+	 * 通知签约\已到店
+	 * @see com.credit.page.ContractNoticePage#Notice()
+	 */
+	public void NoticeBus(String ID){
+		super.pageInNotice();
+		sleep(3000);
+		if(super.searchNotice(ID)){
+			super.notice(ID);
+			if(!searchNotice(ID)){
+				System.out.println("列表中有通知签约记录！");
+				if(this.getText("通知结果状态")=="已通知"){
+					System.out.println("通知签约成功！");
+					super.beenToShop(ID);
+					if(!searchNotice(ID)){
+						System.out.println("已到店成功！");
+					}else{
+						System.out.println("已到店失败！");
+					}
+				}else{
+					System.out.println("通知签约失败！");
+				}
+			}else{
+				System.out.println("列表中无通知签约记录！");
+			}
+		}else{
+			System.out.println("未找到需要签约数据！");
+		}
+
+	}
+
+
+}
---
+++ src/test/java/com/credit/page/ContractAuditPage.java
@@ -0,0 +1,44 @@
+package com.credit.page;
+
+import org.openqa.selenium.WebDriver;
+
+import com.core.utils.Page;
+
+public class ContractAuditPage extends Page {
+	
+	public ContractAuditPage(WebDriver driver) {
+		super(driver);
+	}
+	
+	//进入合同审核页
+	public void PageIn(){
+		driver.navigate().refresh();
+		this.click("合同管理");
+		this.click("合同审核");
+
+	}
+	//查询
+	public Boolean SearchContract(String ID){
+		this.sendKeys("申请编号", ID);
+		this.click("查询");
+		if(this.getText("合同列表")!= ""){
+			System.out.println("显示符合条件查询列表！");
+		}else{
+			System.out.println("无符合条件查询数据！");
+		}
+		return this.getText("合同列表")!= "";
+	}
+	//审核
+	public void Audit(){
+		this.click("进件编号");
+		
+		if(this.windowHandsGet("通过")){
+			this.click("通过");
+		}else{
+			System.out.println("合同审核页面未打开！");
+		}
+		
+		this.click("确定");
+		
+	}
+}
